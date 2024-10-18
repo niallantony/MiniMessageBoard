35191f14486ba030eca33b0a4bd479830d9fae73
Niall Antony <88824327+niallantony@users.noreply.github.com>
2024-10-19 01:17:21 +0900

    Rename Environment Variables

--- model/pool.js
+++ model/pool.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-    host: "localhost",
-    user: process.env.USERNAME,
-    database: "top_users",
-    password: process.env.PASSWORD,
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
     port: 5432
 });
