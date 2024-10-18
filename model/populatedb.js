1a1d7ed304f59026f3b6583caa08fcc75d347a3a
Niall Antony <88824327+niallantony@users.noreply.github.com>
2024-10-19 01:14:51 +0900

    Tweak database settings

--- model/populatedb.js
+++ model/populatedb.js
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS usernames (
     async function main() {
         console.log('seeding...');
         const client = new Client({
-            connectionString: `postgresql://${process.env.USERNAME}:${process.env.PASSWORD}@$localhost:5432/${argv[2]}`,
+            connectionString: `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`,
         });
         await client.connect();
         await client.query(SQL);
